9227c2828f7cde519078930cb5d3e9fd2774bd09
Nonescripter. <nonescripter@gmail.com>
2025-09-09 17:50:36 +0900

    fuck

--- Main.lua
+++ Main.lua
@@ -1,8 +1,16 @@
 local gameremotes = game.ReplicatedStorage.GameRemotes
 local Demo = gameremotes:FindFirstChild("Demo") or game.Workspace:FindFirstChild("Demo")
 local DupeModeVal = 'Selected'
+local strafeRange
+local run = function(func)
+    func()
+end
+local Players = game:GetService("Players")
 local player = game:GetService("Players").LocalPlayer
-local Sel = player.PlayerGui.HUDGui.Inventory.Slots:FindFirstChild("Slot-1")
+local slot = player.PlayerGui.HUDGui.Inventory.Slots:FindFirstChild("Slot-1")
+local moveitems = gameremotes:FindFirstChild("MoveItem") or gameremotes:FindFirstChild("MoveItems")
+local fluidFolder = Workspace:FindFirstChild("Fluid")
+
 
 shared.VapeIndependent = true
 local vape = loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua', true))()
@@ -12,6 +20,8 @@ loadfile('newvape/games/Universal.lua')()
 local function notif(...)
     return vape:CreateNotification(...)
 end
+
+notif('From Nonescripter', 'I\'m sorry, i was lazy to add much module.')
 
 local NoFall = vape.Categories.Blatant:CreateModule({
     Name = 'NoFall',
@@ -24,15 +34,16 @@ local NoFall = vape.Categories.Blatant:CreateModule({
             else Demo.Parent = game.Workspace end
         end
     end,
+    ExtraText = function() return 'NoRemote' end,
     Tooltip = 'Removes fall damage.'
 })
 
 local Dupe = vape.Categories.Inventory:CreateModule({
-    Name = 'Dupe (WIP)',
+    Name = 'Dupe (Broken BTW)',
     Function = function(callback)
         if callback then
             if DupeModeVal == 'Selected' then
-                local b = Sel.SlotNA.Count
+                local b = slot.SlotNA.Count
                 local bCount = tonumber(b.Text)
                 if not bCount then
                     return
@@ -52,11 +63,11 @@ local Dupe = vape.Categories.Inventory:CreateModule({
                         moveitems:InvokeServer(-1, 82, true, -howmuch)
                     end
                 end)
+                notif('Dupe', 'Duped item using ' .. DupeModeVal .. ' method.', 3)
             end
-            if err <= 1 then notif('Dupe', 'Error.', 3, 'Warning')
-            else notif('Dupe', 'Duped item using ' .. DupeModeVal .. ' method.', 3) end
         end
     end,
+    ExtraText = function() return DupeModeVal end,
     Tooltip = 'Dupe items.'
 })
 
@@ -65,12 +76,21 @@ DupeMode = Dupe:CreateDropdown({
     List = {'Selected', ':)'},
     Function = function(val)
         DupeModeVal = val
-        if val == 'Selected' then
-            notif('Dupe', 'Open chest, Select item and enable dupe', 6)
+        if val == 'Selected' then notif('Dupe', 'Open chest, Select item and enable dupe', 6) end
     end,
     Tooltip = 'Method of duping.'
 })
 
-vape:Init()
+local FastBreak = vape.Categories.World:CreateModule({
+    Name = 'FastBreak',
+    Function = function(callback)
+        while callback do
+            gameremotes.AcceptBreakBlock:InvokeServer()
+            task.wait()
+        end
+    end,
+    ExtraText = function() return 'RemoteSpam' end,
+    Tooltip = 'Break blocks fast if you have right tool.'
+})
 
--- i tried my best ahh
+vape:Init()
