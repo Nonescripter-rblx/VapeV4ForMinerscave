152c3530e79254fbed3da991f7287a9518f56a01
Nonescripter. <nonescripter@gmail.com>
2025-09-10 14:06:08 +0900

    Killaura working :D

--- Main.lua
+++ Main.lua
@@ -131,7 +131,7 @@ lol(function()
     local Attack = game:GetService('ReplicatedStorage'):FindFirstChild('GameRemotes').Attack
     local LocalPlayer = game:GetService('Players').LocalPlayer
     local Players = game:GetService('Players')
-	local SearchDistance = 16
+    local SearchDistance = 16
     local status = 'Waiting'
     local Target
     local Method = 'Health'
@@ -146,66 +146,92 @@ lol(function()
 
         for _, Search in ipairs(Players:GetPlayers()) do
             if Search ~= LocalPlayer and Search.Character then -- if target isn't localplayer and target is alive
-                local SearchRoot = Search:FindFirstChild("HumanoidRootPart")
+                local SearchRoot = Search.Character:FindFirstChild("HumanoidRootPart")
                 local SearchHumanoid = Search.Character:FindFirstChild("Humanoid")
                 
                 if SearchHumanoid and SearchRoot and SearchHumanoid.Health > 0 then
                     local Distance = (SearchRoot.Position - LocalRoot.Position).Magnitude
 
-					if Distance <= SearchDistance and SearchHumanoid.Health < Lowest then
-						Lowest = SearchHumanoid.Health
-						CurrentTarget = Search
-					end
-				end
-			end
-		end
-        warn(CurrentTarget.Character.Humanoid.Name)
-		return CurrentTarget
-	end
+                    if Distance <= SearchDistance and SearchHumanoid.Health < Lowest then
+                        Lowest = SearchHumanoid.Health
+                        CurrentTarget = Search
+                    end
+                end
+            end
+        end
+        
+        return CurrentTarget
+    end
+
+    local function GetDistanceTarget()
+        local Closest = math.huge
+        local CurrentTarget = nil
+        local LocalRoot = Players.LocalPlayer.Character and Players.LocalPlayer.Character:FindFirstChild('HumanoidRootPart')
+
+        if not LocalRoot then return nil end
+
+        for _, Search in ipairs(Players:GetPlayers()) do
+            if Search ~= LocalPlayer and Search.Character then -- if target isn't localplayer and target is alive
+                local SearchRoot = Search.Character:FindFirstChild("HumanoidRootPart")
+                local SearchHumanoid = Search.Character:FindFirstChild("Humanoid")
+                
+                if SearchHumanoid and SearchRoot and SearchHumanoid.Health > 0 then
+                    local Distance = (SearchRoot.Position - LocalRoot.Position).Magnitude
+
+                    if Distance <= SearchDistance and Distance < Closest then
+                        Closest = Distance
+                        CurrentTarget = Search
+                    end
+                end
+            end
+        end
+        
+        return CurrentTarget
+    end
 
     KillAura = vape.Categories.Blatant:CreateModule({
         Name = 'KillAura',
         Function = function(callback)
             while KillAura.Enabled do
                 if Method == 'Health' then Target = GetHealthTarget()
-                elseif Method == 'Distance' then Target = GetHealthTarget() end
+                elseif Method == 'Distance' then Target = GetDistanceTarget() end
 
                 if Target and Target.Character then
                     local Root = Target.Character:FindFirstChild('HumanoidRootPart')
                     if Root and LocalPlayer:DistanceFromCharacter(Root.Position) <= SearchDistance then
                         Attack:InvokeServer(Target.Character)
-                        status = 'Attacking'
                     end
-                else status = 'Waiting' end
+                end
                 task.wait(0.3)
             end
         end,
-        ExtraText = function() return status end,
+        ExtraText = function() return Method end,
         Tooltip = 'Attack Enemy Without hitting them.'
     })
 
-	RangeSlider = KillAura:CreateSlider({
-		Name = 'Range',
-		Min = 0,
-		Max = 16,
-		Function = function(val)
-			SearchDistance = val
-		end,
-		Suffix = function(val)
-			return val == 1 and 'stud' or 'studs'
-		end,
-		Tooltip = 'Range to Attack'
-	})
+    RangeSlider = KillAura:CreateSlider({
+        Name = 'Range',
+        Min = 0,
+        Max = 16,
+        Function = function(val)
+            SearchDistance = val
+        end,
+        Suffix = function(val)
+            return val == 1 and 'stud' or 'studs'
+        end,
+        Tooltip = 'Range to Attack'
+    })
 
-	RangeSlider:SetValue(16)
+    RangeSlider:SetValue(16)
 
-	dropdown = KillAura:CreateDropdown({
-		Name = 'Target',
-		List = {'Health', 'Distance'},
-		Function = function(val)
-			Method = val
-		end,
-		Tooltip = 'Target to Attack'
-	})
+    dropdown = KillAura:CreateDropdown({
+        Name = 'Target',
+        List = {'Health', 'Distance'},
+        Function = function(val)
+            Method = val
+        end,
+        Tooltip = 'Target to Attack'
+    })
 end)
+
 vape:Init()
